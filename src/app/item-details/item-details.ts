92ef49dfc246ba4481a3b4ec9b7b791b477d205c
What-1s-love <belikov.vlad.2016@gmail.com>
2025-11-28 18:42:08 +0100

    fix: update article model and service to support string IDs

--- src/app/item-details/item-details.ts
+++ src/app/item-details/item-details.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router'; // Для читання URL
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Article } from '../shared/models/article.model';
 import { DataService } from '../shared/services/data';
 import { CommonModule } from '@angular/common';
@@ -7,22 +7,21 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-item-details',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './item-details.html',
   styleUrls: ['./item-details.css']
 })
 export class ItemDetails implements OnInit {
-
-  article?: Article;
+  article: Article | undefined;
 
   constructor(
-    private route: ActivatedRoute, 
-    private dataService: DataService 
+    private route: ActivatedRoute,
+    private dataService: DataService
   ) {}
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-
+    const id = this.route.snapshot.paramMap.get('id');
+    
     if (id) {
       this.article = this.dataService.getArticleById(id);
     }
